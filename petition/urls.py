430a997e276a459f74d44ce09036a20e97061050
sjhuang26 <sjhuang26@gmail.com>
2019-02-07 19:37:24 -0500

    Work on frontend
    Routes:
    / (index)
    /about
    /login
    /petitions/<id>
    /tags/<tag>
    /petitions/create
    /login

--- petition/urls.py
+++ petition/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from petition import views
+from . import views
 
+app_name='petition'
 urlpatterns = [    
     path('', views.index, name='index'),
-    path('view', views.view_all, name='view_all')
+    path('petitions/<int:petition_id>', views.view_petition, name='view_petition'),
+    path('about', views.about, name='about'),
+    path('petitions/create', views.create_petition, name='create_petition'),
+    path('login', views.login, name='login'),
+    path('tags/<slug:tag_label>', views.view_tag, name='view_tag'),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
--- petition/views.py
+++ petition/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from petition.models import Petition
+from django.shortcuts import render, get_object_or_404
+from .models import Petition
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'petitions': Petition.objects.all()})
 
-def view_all(request):
-    return render(request, 'view_all.html')
+def view_petition(request, petition_id):
+    return render(request, 'view-petition.html', {'petition': get_object_or_404(Petition, pk=petition_id)})
 
-class PetitionGridView():
-    model = Petition
-    context_object_name = 'petitions'
-    # queryset = Petition.objects.all()
+def about(request):
+    return render(request, 'about.html')
+
+def create_petition(request):
+    return render(request, 'create-petition.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def view_tag(request, tag_label):
+    return render(request, 'view-tag.html')
