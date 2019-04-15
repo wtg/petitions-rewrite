7a2420808ce74dce5cba77b1f13c45e3aa310776
andlas98 <andrewohakam@outlook.com>
2019-04-15 15:29:32 -0400

    Part 1 of pull request changes(removing extraneous comments, for loop fixes,implementing suggestions,etc )

--- index/urls.py
+++ index/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("all/", views.all, name="all"),
     path("about/", views.about, name="about")
-    #path("about/#moderation/", views.moderation, name="moderation")
 ]
